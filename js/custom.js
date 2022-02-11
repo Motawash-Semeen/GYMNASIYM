2b7ed355177e764c7ac4ae6dbda3752675e8e478
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2022-02-11 13:33:03 +0600

    lasst

--- js/custom.js
+++ js/custom.js
@@ -40,5 +40,76 @@ $('.slick-prev').html('<i class="fas fa-chevron-left" aria-hidden="true" ></i>')
 $(function () {
   $('ul.tabs').pixiefyTabs();
 })
+$(document).ready(function() {
+  $('.stickyMenu').smoothMenu({
+    stickyMenu: true,
+    slidingLine: true
+  });
+});
 
 
+/* SCROLL TO TOP START */
+
+$(document).ready(function() {
+
+  $.apScrollTop({
+      'onInit': function(evt) {
+          console.log('apScrollTop: init');
+      }
+  });
+
+  // Add event listeners
+  $.apScrollTop().on('apstInit', function(evt) {
+      console.log('apScrollTop: init');
+  });
+
+  $.apScrollTop().on('apstToggle', function(evt, details) {
+      console.log('apScrollTop: toggle / is visible: ' + details.visible);
+  });
+
+  $.apScrollTop().on('apstCssClassesUpdated', function(evt) {
+      console.log('apScrollTop: cssClassesUpdated');
+  });
+
+  $.apScrollTop().on('apstPositionUpdated', function(evt) {
+      console.log('apScrollTop: positionUpdated');
+  });
+
+  $.apScrollTop().on('apstEnabled', function(evt) {
+      console.log('apScrollTop: enabled');
+  });
+
+  $.apScrollTop().on('apstDisabled', function(evt) {
+      console.log('apScrollTop: disabled');
+  });
+
+  $.apScrollTop().on('apstBeforeScrollTo', function(evt, details) {
+      console.log('apScrollTop: beforeScrollTo / position: ' + details.position + ', speed: ' + 10000);
+
+      // You can return a single number here, which means that to this position
+      // browser window scolls to
+      /*
+      return 100;
+      */
+
+      // .. or you can return an object, containing position and speed:
+      /*
+      return {
+          position: 100,
+          speed: 100
+      };
+      */
+
+      // .. or do not return anything, so the default values are used to scroll
+  });
+
+  $.apScrollTop().on('apstScrolledTo', function(evt, details) {
+      console.log('apScrollTop: scrolledTo / position: ' + details.position);
+  });
+
+  $.apScrollTop().on('apstDestroy', function(evt, details) {
+      console.log('apScrollTop: destroy');
+  });
+
+});
+/* SCROLL TO TOP END */
